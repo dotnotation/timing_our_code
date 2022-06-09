505f9f6a29c76cbacda0cf12f61c139951401368
dotnotation <dotinmotionproductions@gmail.com>
2022-06-09 12:06:13 -0400

    added slow add up to solution

---
+++ src/add_up_to_slower.js
@@ -0,0 +1,9 @@
+// write a function that calculates the sum of all numbers from 1 up to and including some number n
+
+function addUpTo(n){
+    let total = 0
+    for (let i = 1; i <= n; i++){
+        total += i
+    }
+    return total
+}
