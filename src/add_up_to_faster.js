979f51b35a2bc5f6a825783592304ec51ad89627
dotnotation <dotinmotionproductions@gmail.com>
2022-06-09 14:10:46 -0400

    added performance timer/finished this code along

--- src/add_up_to_faster.js
+++ src/add_up_to_faster.js
@@ -1,3 +1,8 @@
-function addUpTo(n){
+function addUpToFast(n){
     return n * (n + 1) / 2
 }
+
+let time1 = performance.now()
+addUpToFast(1000000000)
+let time2 = performance.now()
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
--- src/add_up_to_slower.js
+++ src/add_up_to_slower.js
@@ -1,9 +1,14 @@
 // write a function that calculates the sum of all numbers from 1 up to and including some number n
 
-function addUpTo(n){
+function addUpToSlow(n){
     let total = 0
     for (let i = 1; i <= n; i++){
         total += i
     }
     return total
 }
+
+let t1 = performance.now()
+addUpToSlow(1000000000)
+let t2 = performance.now()
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
