3fb25fa4ae28cad7083c4eb405b7517f901bc6ca
dotnotation <dotinmotionproductions@gmail.com>
2022-06-10 16:16:22 -0400

    added notes from lecture

--- src/add_up_to_faster.js
+++ src/add_up_to_faster.js
@@ -1,3 +1,5 @@
+// write a function that calculates the sum of all numbers from 1 up to and including some number n
+
 function addUpToFast(n){
     return n * (n + 1) / 2
 }
@@ -6,3 +8,7 @@ let time1 = performance.now()
 addUpToFast(1000000000)
 let time2 = performance.now()
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+// the best way to calculate performance is by counting the number of operations
+// this solution only has three operations
+// the operations are *, +, and /
--- src/add_up_to_slower.js
+++ src/add_up_to_slower.js
@@ -12,3 +12,9 @@ let t1 = performance.now()
 addUpToSlow(1000000000)
 let t2 = performance.now()
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+// the best way to calculate performance is by counting the number of operations
+// this solution has many operations based on how many times the loop is run
+// for the single line total += i, the operations are + and =(assignment) but the amount of times that runs is based on n
+// if n = 5, then there are five operations, but if n = 100 then there are 100 operations
+// however there are even more operations through the whole function
